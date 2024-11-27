0fd8424f3ada4c6ad0247791adba9c23419d147d
diegofiberux <diego@marmentini.net>
2024-11-27 14:21:51 -0300

    push test

--- cademi_script.js
+++ cademi_script.js
@@ -1,2 +1,2 @@
 console.log("hey cademi ... is Diego from local VSCODE here o/")
-document.write("hey there from document dot write asedehee")
+document.write("hey there from document dot write traaaaaaaaaaaaaaa")
