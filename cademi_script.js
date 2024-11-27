704812c41b2bb9414c970f615a91007a474ae874
diegofiberux <diego@marmentini.net>
2024-11-27 14:05:24 -0300

    second comit

--- cademi_script.js
+++ cademi_script.js
@@ -1,2 +1,2 @@
 console.log("hey cademi ... is Diego from local VSCODE here o/")
-document.write("hey there from document dot write uiaaa")
+document.write("hey there from document dot write asedehee")
